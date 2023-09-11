501b273e0d57cad1dfc49c6ede0f380a40678b3b
Bibek1500 <80021162+Bibek1500@users.noreply.github.com>
2023-09-11 12:41:13 +1000

    F1-Input the assignment name.

---
+++ StudentStatistics.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+/**
+ * Write a description of class StudentStatistics here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class StudentStatistics
+{
+      public static void main(String[] args){
+    //f1\
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter the Assessment name: ");
+    String assessmentName = input.nextLine();
+    
+}} 
