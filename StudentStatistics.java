8d67136898edfc5f7d6f97a9c4a23ce7ac76ade5
Bibek1500 <80021162+Bibek1500@users.noreply.github.com>
2023-09-11 12:46:35 +1000

    F2 and F3 - Allow the user to enter the student marks and show validation.

--- StudentStatistics.java
+++ StudentStatistics.java
@@ -13,4 +13,17 @@ public class StudentStatistics
     System.out.println("Enter the Assessment name: ");
     String assessmentName = input.nextLine();
     
+     //f2 and f3
+    double [] marks = new double[3];
+    for (int i = 0; i<marks.length;i++){
+        double mark;
+        do{
+            System.out.println("enter the mark: ");
+            mark = input.nextDouble();
+            if (mark<0 || mark>30){
+                System.out.print("Error: The mark should be between 0 and 30!");
+            }
+        }while(mark <0 || mark>30);
+    marks[i] = mark;
+    }
 }} 
